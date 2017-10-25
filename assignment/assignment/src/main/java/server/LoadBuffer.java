54dc49d7958754447ac04004a20f8e20ecb41170
Moyang He <moyang@Moyangs-MacBook-Pro-2.local>
2017-10-24 19:19:27 -0700

    remove inner class

--- assignment/assignment/src/main/java/server/LoadBuffer.java
+++ assignment/assignment/src/main/java/server/LoadBuffer.java
@@ -14,7 +14,19 @@ public class LoadBuffer {
     private static final BlockingQueue<RFIDLiftData> buffer = new LinkedBlockingQueue<>();
 
     static {
-        new LoadHandler(buffer).start();
+//        new LoadHandler(buffer).start();
+        new Thread() {
+            @Override
+            @SneakyThrows
+            public void run() {
+                while (true) {
+                    RFIDLiftData r = buffer.take();
+                    Connection conn = DBConnectionPoolWrapper.getConnection();
+                    new RFIDLiftDataDAO(conn).load(r);
+                    conn.close();
+                }
+            }
+        }.start();
     }
 
     static void put(RFIDLiftData r) {
@@ -22,18 +34,18 @@ public class LoadBuffer {
     }
 }
 
-@AllArgsConstructor
-class LoadHandler extends Thread {
-    private final BlockingQueue<RFIDLiftData> queue;
-
-    @Override
-    @SneakyThrows
-    public void run() {
-        while (true) {
-            RFIDLiftData r = queue.take();
-            Connection conn = DBConnectionPoolWrapper.getConnection();
-            new RFIDLiftDataDAO(conn).load(r);
-            conn.close();
-        }
-    }
-}
+//@AllArgsConstructor
+//class LoadHandler extends Thread {
+//    private final BlockingQueue<RFIDLiftData> queue;
+//
+//    @Override
+//    @SneakyThrows
+//    public void run() {
+//        while (true) {
+//            RFIDLiftData r = queue.take();
+//            Connection conn = DBConnectionPoolWrapper.getConnection();
+//            new RFIDLiftDataDAO(conn).load(r);
+//            conn.close();
+//        }
+//    }
+//}
